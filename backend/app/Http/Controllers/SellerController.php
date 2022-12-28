b8729f7b512306ac9d84efbc7b0c6d9df794ef8b
Emanuelly <emanuellyvcarv@gmail.com>
2022-12-28 16:25:52 -0300

    wip(seller): implementing controller

---
+++ backend/app/Http/Controllers/SellerController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreSellerRequest;
+use App\Http\Requests\UpdateSellerRequest;
+use App\Models\Seller;
+
+class SellerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Seller::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreSellerRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreSellerRequest $request)
+    {
+        $works = Seller::create($request->validated());
+        if($works){
+            return response()->json([
+                'status' => 200,
+            ]);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Seller  $seller
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Seller $seller)
+    {
+        return $seller;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateSellerRequest  $request
+     * @param  \App\Models\Seller  $seller
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateSellerRequest $request, Seller $seller)
+    {
+        $works = $seller->update($request->validated());
+        if($works){
+            return response()->json([
+                'status' => 200,
+            ]);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Seller  $seller
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Seller $seller)
+    {
+        $works = $seller->destroy();
+        if($works){
+            return response()->json([
+                'status' => 200,
+            ]);
+        }
+    }
+}
