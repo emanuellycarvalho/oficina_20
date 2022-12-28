c5b099e0f594afd293fd5485afa292c6e6c2a071
Emanuelly <emanuellyvcarv@gmail.com>
2022-12-28 12:51:25 -0300

    wip(database): creating migrations

---
+++ backend/database/migrations/tables/2022_12_28_151537_create_budgets_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('budgets', function (Blueprint $table) {
+            $table->id();
+            $table->string('client', 100);
+            $table->longText('description');
+            $table->double('value', 10, 2);
+            $table->foreignId('seller_id')->constrained('sellers');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('budgets');
+    }
+};
