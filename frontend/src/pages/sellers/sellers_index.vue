fdd04995f1e362e5a4cab865f76b3772505fad71
Emanuelly <emanuellyvcarv@gmail.com>
2022-12-28 20:24:37 -0300

    wip(page): implementing sellers index

---
+++ frontend/src/pages/sellers/sellers_index.vue
@@ -0,0 +1,20 @@
+<template>
+    <q-page class="flex flex-center">
+        <DataTableConteudos :sellers="sellers"></DataTableConteudos>
+    </q-page>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+import { useSellerStore } from "src/stores/seller";
+import DataTableConteudos from "/src/components/DataTableSellers.vue"
+
+const sellerStore = useSellerStore();
+const sellers = ref(null);
+
+onMounted(() =>{
+    sellers.value = sellerStore.getActiveSellers();
+})
+
+</script>
+  
