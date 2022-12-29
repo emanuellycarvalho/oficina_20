2a71f088d924b8a3748c2a005d36f3eeed26bb84
Emanuelly <emanuellyvcarv@gmail.com>
2022-12-28 22:41:33 -0300

    wip(seller): adding calculated attribute to model

--- backend/app/Models/Seller.php
+++ backend/app/Models/Seller.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 
 class Seller extends Model
@@ -21,7 +22,14 @@ class Seller extends Model
         'updated_at'
     ];
 
+    protected $appends = ['budgetAmount'];
+
     public function budgets(){
         return $this->hasMany(Budget::class);
+    }
+
+    protected function getBudgetAmountAttribute()
+    {
+        return $this->budgets()->count();
     }
 }
