793193e34c8dc39b752686b2bb0cc6ac568392fb
Emanuelly <emanuellyvcarv@gmail.com>
2022-12-29 00:20:27 -0300

    wip(pages): implementing budget's index

---
+++ frontend/src/pages/budgets/budgets_index.vue
@@ -0,0 +1,21 @@
+<template>
+    <q-page class="flex flex-center">
+        <DataTableBudgets :budgets="budgets"></DataTableBudgets>
+    </q-page>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+import { useBudgetStore } from "src/stores/budget";
+import DataTableBudgets from "/src/components/DataTableBudgets.vue";
+
+const budgetStore = useBudgetStore();
+const budgets = ref([]);
+
+onMounted(() =>{
+    budgetStore.getBudgets();
+    budgets.value = budgetStore.budgets;
+})
+
+</script>
+  
